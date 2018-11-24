3b9698faf50e1ec905c1c65b68b64ac5bb5ae016
tocomi <tocomi@example.com>
2018-11-24 17:29:12 +0900

    get grade

--- netkeiba_scraper/netkeiba_scraper/spiders/umaaji_calculator.py
+++ netkeiba_scraper/netkeiba_scraper/spiders/umaaji_calculator.py
@@ -12,14 +12,13 @@ class UmaajiCalculatorSpider(scrapy.Spider):
 
     def parse(self, response):
         # 本日のレース一覧からレース情報URLをパース #
-        for url in response.css('.RaceList_Box dt a::attr(href)').re(r'/\?pid.*'):
-            yield scrapy.Request(self.start_urls[0] + url, self.parse_race)
+        for url in response.css('.RaceList_Box dt a::attr(href)').re(r'/\?pid.*race.*'):
+            yield scrapy.Request(self.start_urls[0] + re.sub("mode=top", 'mode=shutuba', url), self.parse_horse)
 
     def parse_race(self, response):
         # レース情報から馬柱へのリンクをパース #
         for url in response.css('.sub_menu a::attr(href)').re(r'/\?pid.*mode=shutuba'):
             yield scrapy.Request(self.start_urls[0] + url, self.parse_horse)
-
 
     def parse_horse(self, response):
         result = self.get_race_data(response)
@@ -50,10 +49,29 @@ class UmaajiCalculatorSpider(scrapy.Spider):
         past_race_html_list = horse.css('.txt_l')[2:7]
         for past_race_html in past_race_html_list:
             past_race = {}
-            diff = past_race_html.css('.h_name_01::text').extract_first()
-            past_race['diff'] = re.sub(r"(\(|\))", '', diff) if diff != None else diff
+            past_race['grade'] = self.get_grade(past_race_html)
+            past_race['diff'] = self.get_diff(past_race_html)
             past_races.append(past_race)
 
         horse_data['past_races'] = past_races
 
         return horse_data
+
+    def get_grade(self, past_race_html):
+        grade = past_race_html.css('.race_grade::text').extract_first()
+        if grade != None:
+            return grade
+
+        grade = past_race_html.css('.race_name a::text').extract_first()
+        if grade == None:
+            return ''
+        regexp = re.compile("([０-９]{3,4}|未勝利|新馬)")
+        match = regexp.search(grade)
+        return match.group(0) if match != None else ''
+
+    def parse_grade(self, grade):
+        return grade;
+
+    def get_diff(self, past_race_html):
+        diff = past_race_html.css('.h_name_01::text').extract_first()
+        return re.sub(r"(\(|\))", '', diff) if diff != None else diff
